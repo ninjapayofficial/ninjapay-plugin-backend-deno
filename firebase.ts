a15a8e2f11c91b4cc7cf958322c3ad79996df305
dev <dev@ninjapay.me>
2024-11-30 13:55:30 +0000

    provider adding working for opennode

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,13 @@
 // firebase.js
+
+// Firebase Admin SDK for Deno
+// import { initializeApp, cert, ServiceAccount } from "https://esm.sh/firebase-admin@11.5.0/app";
+// import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js";
+// import { initializeApp, cert, getAuth, getFirestore } from "https://deno.land/x/firebase_adminsdk@v0.5.0/mod.ts";
+// import firebase from "https://esm.sh/firebase@9.17.0/app";
 import admin from "npm:firebase-admin@12.7.0";
-import { config } from "npm:dotenv@16.3.1";
+import { config } from "npm:dotenv";
 // Load environment variables from .env
 config();
 import { ServiceAccount } from "https://esm.sh/firebase-admin@11.5.0/app";
--- main.ts
+++ main.ts
@@ -11,27 +11,11 @@ import {
   getCookies,
   setCookie,
 } from "https://deno.land/std@0.224.0/http/cookie.ts";
-// Firebase Admin SDK for Deno
-// import { initializeApp, cert, ServiceAccount } from "https://esm.sh/firebase-admin@11.5.0/app";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js";
-// import { initializeApp, cert, getAuth, getFirestore } from "https://deno.land/x/firebase_adminsdk@v0.5.0/mod.ts";
-// import firebase from "https://esm.sh/firebase@9.17.0/app";
+
 import admin from "./firebase.ts";
 // Load environment variables from .env
 config();
 
-// Initialize Firebase Admin SDK
-// const serviceAccount = {
-//   projectId: Deno.env.get("FIREBASE_PROJECT_ID"),
-//   clientEmail: Deno.env.get("FIREBASE_CLIENT_EMAIL"),
-//   privateKey: Deno.env.get("FIREBASE_PRIVATE_KEY")?.replace(/\\n/g, "\n"),
-// };
-// // import serviceAccount from './ninjapay-plugin-backend-dev-firebase-adminsdk-oiyn8-3f93833a6b.json' with { type: "json" }; // Replace with the path to your service account key
-// // const { project_id, private_key_id, private_key, client_email, client_id, auth_uri, token_uri, auth_provider_x509_cert_url, client_x509_cert_url } = serviceAccount;
-// initializeApp({
-//   credential: cert(serviceAccount),
-// });
 
 const auth = admin.auth();
 const db = admin.firestore();
